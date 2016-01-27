f6085ca7da047d57bd1c553029a6a0041fe94860
fpierrel01 <fpierrel@optonline.net>
2016-01-27 00:27:09 -0500

    average daily activity pattern

--- PA1_template.R
+++ PA1_template.R
@@ -16,6 +16,29 @@
         median(total.steps, na.rm=TRUE)
         ### The median is [1] 10395
         
+# Average daily Activity pattern
+##Time series plot of 5 minute interval
+        library(ggplot2)
+        averages <- aggregate(x=list(steps=data$steps), by=list(interval=data$interval),
+                              FUN=mean, na.rm=TRUE)
+        ggplot(data=averages, aes(x=interval, y=steps)) +
+                geom_line() +
+                xlab("5-minute interval") +
+                ylab("average number of steps taken")        
+        
+## The 5-minute interval , on average, that contains the maximum number of steps        
+        averages[which.max(averages$steps),]
+        
+        ###     interval    steps
+        ### 104      835 206.1698
+
+# Imputing missing values       
+## Calculate the total number of missing values
+        missing <- is.na(data$steps)
+        table(missing)        
+        ### missing
+        ### FALSE  TRUE 
+        ### 15264  2304
         
         
         
